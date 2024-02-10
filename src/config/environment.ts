743c7abb92a00c2e5e20ba6633ef5909cebb7533
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-10 16:13:17 -0500

    Fix npm start error

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,3 +1,7 @@
+import ip from 'ip';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 let ENV_VARIABLES : {
     production: boolean, 
     apiUrl: string, 
@@ -5,25 +9,22 @@ let ENV_VARIABLES : {
     stage_environement: string
  };
  
-let BASE_PATH = process.env.BASE_URL;
-import ip from 'ip';
-
-// Get the local IP address
+let API_BASE_PATH = process.env.BASE_URL;  // v1/api
 const localIp = ip.address();
 const PORT = process.env.PORT || 5050;
 
-if (process.env.NODE_ENV === 'development') {  //development config
+if (process.env.NODE_ENV === "development") {  //development config
     ENV_VARIABLES = {
         production: false,
         apiUrl: `http://localhost:${PORT}`,
-        apiUrlPath: `http://localhost:${PORT}${BASE_PATH}`,
+        apiUrlPath: `http://localhost:${PORT}${API_BASE_PATH}`,
         stage_environement: "development"
     };
 } else { //production config
     ENV_VARIABLES = {
         production: true,
         apiUrl: 'http://production-api-url.com',
-        apiUrlPath: `http://localhost:${PORT}${BASE_PATH}`,
+        apiUrlPath: `http://localhost:${PORT}${API_BASE_PATH}`,
         stage_environement: "production"
     };
 }
