7a8a67d963941de6f0b92b7af1b0666bb4f48a20
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-17 01:01:02 -0500

    Testing bash script dev

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/project/event-app-backend
+#cd /home/ubuntu/project/event-app-backend
 
 #start the application
-pm2 start npm --name eventapp -- start
+#pm2 start npm --name eventapp -- start
