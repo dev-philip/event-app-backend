1f648cad7eb55085a58ef57d216976dcaf7f9468
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-18 03:59:52 -0500

    automation

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#Stop pm2 Process
+#pm2 stop all
+
+
 #add configuration files from tmpappconfig which are config.json and .env file
-#sudo cp -a /home/ubuntu/event-app-backend/tmpappconfig/. /home/ubuntu/event-app-backend/
+sudo cp -a /home/ubuntu/event-app-backend/tmpappconfig/. /home/ubuntu/event-app-backend/
 
 #delete tmp folder
-#sudo rm -rf /home/ubuntu/event-app-backend/tmpappconfig
+sudo rm -rf /home/ubuntu/event-app-backend/tmpappconfig
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#Stop pm2 Process
-#pm2 stop all
 
 # Clean up old Artifact
 sudo rm -rf /home/ubuntu/event-app-backend
