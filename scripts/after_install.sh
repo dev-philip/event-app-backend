cefcb5907f1471cfd2537c6e1604cd9770e48e1b
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-18 16:07:45 -0500

    automation test well 2

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -9,7 +9,3 @@ sudo cp -a /home/ubuntu/event-app-backend/tmpappconfig/. /home/ubuntu/event-app-
 
 #delete tmp folder
 sudo rm -rf /home/ubuntu/event-app-backend/tmpappconfig
-
-
-#This command is used to change the ownership of the /home/ubuntu/event-app-backend directory and all its contents recursively
-sudo chown -R $(whoami) /home/ubuntu/event-app-backend
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
+#This command is used to change the ownership of the /home/ubuntu/event-app-backend directory and all its contents recursively
+sudo chown -R $(whoami) /home/ubuntu/event-app-backend
+
 #start the application
-#cd event-app-backend/
+cd event-app-backend/
+
+#remove node module 
+sudo rm -rf node_modules
+
+#install app packages
+npm install
 
 #use pm2 to start the node js application
-#pm2 start npm --name eventapp -- start
+pm2 start npm --name eventapp -- start
