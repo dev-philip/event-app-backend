9d089179992414e90a39eaaf47c85182506e25b8
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-18 17:45:05 -0500

    automation test well 6

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Stop pm2 Process
+#pm2 stop all
 
 #add configuration files from tmpappconfig which are config.json and .env file
 sudo cp -a /home/ubuntu/event-app-backend/tmpappconfig/. /home/ubuntu/event-app-backend/
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,5 +4,3 @@
 # Clean up old Artifact
 sudo rm -rf /home/ubuntu/event-app-backend
 
-#Stop pm2 Process
-pm2 stop all
