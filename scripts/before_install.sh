1e2f87a5ad8cdeb81d973f65e6442c70dc08a9f0
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-18 18:36:30 -0500

    automation test well 7

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,4 +5,4 @@
 sudo rm -rf /home/ubuntu/event-app-backend
 
 #This command is used to change the ownership of the /home/ubuntu/event-app-backend directory and all its contents recursively
-sudo chown -R $(whoami) /home/ubuntu/event-app-backend
+#sudo chown -R $(whoami) /home/ubuntu/event-app-backend
