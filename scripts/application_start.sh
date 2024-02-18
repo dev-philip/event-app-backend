b5bad49f349173571dc84df4346d19eb7cb48d55
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-18 04:06:21 -0500

    automation

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #start the application
-#cd event-app-backend/
-#pm2 start npm --name eventapp -- start
+cd event-app-backend/
+
+#use pm2 to start the node js application
+pm2 start npm --name eventapp -- start
