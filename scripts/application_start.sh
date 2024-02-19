2337f8a132af3a1f70e10f7c5c84360eca8881e9
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-19 00:59:49 -0500

    CI/CD pipe

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#start new approach
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#end of new approach
+
 #cd /home/ubuntu/event-app-backend && npm install > /tmp/nmpinstalloutputola.txt 2>/tmp/npminstallerrorola.txt
 
 
@@ -11,4 +33,4 @@
 #npm install
 
 #use pm2 to start the node js application
-pm2 start npm --name eventapp -- start > /tmp/pm2startoutputola.txt 2>/tmp/pm2starterrorola.txt
+#pm2 start npm --name eventapp -- start > /tmp/pm2startoutputola.txt 2>/tmp/pm2starterrorola.txt
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
 
+#start new approach
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#end of new approach
+
+
 # Clean up old Artifact
-sudo rm -rf /home/ubuntu/event-app-backend
+#sudo rm -rf /home/ubuntu/event-app-backend
 
 #This command is used to change the ownership of the /home/ubuntu/event-app-backend directory and all its contents recursively
 #sudo chown -R $(whoami) /home/ubuntu/event-app-backend
